9a6af6b5894e24f7a402a420e57b8b7106b2d7c4
Gerard Forcada <gerardforcada@gmail.com>
2021-04-11 04:50:41 +0200

    ID Republicana Migration & Publishes Tags

--- src/Providers/ConsellRepublicaProvider.php
+++ src/Providers/ConsellRepublicaProvider.php
@@ -44,12 +44,17 @@ class ConsellRepublicaProvider extends ServiceProvider
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'consellrep');
 
         $this->publishes([
-            __DIR__.'/../../resources/lang' => resource_path('lang/vendor/consellrep/'),
-        ]);
+            __DIR__ . '/../../resources/lang' => resource_path('lang/vendor/consellrep/'),
+        ], 'consellrep-translations');
 
         $this->publishes([
-            __DIR__.'/../../config/cxr.php' => config_path('cxr.php'),
-        ]);
+            __DIR__ . '/../../config/cxr.php' => config_path('cxr.php'),
+        ], 'consellrep-config');
+
+        $this->publishes([
+            __DIR__ . '/../../database/migrations/' => database_path('migrations')
+        ], 'consellrep-migrations');
+
     }
 
     /**
